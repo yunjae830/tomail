9ea8b4d2582ce30da3a58ff42d20a736a2b9472d
yunjae830 <jea830@naver.com>
2019-01-21 08:47:20 +0900

    디비 컬럼추가 (emailchecked)

--- map/src/main/webapp/WEB-INF/spring/sqls/db.sql
+++ map/src/main/webapp/WEB-INF/spring/sqls/db.sql
@@ -7,6 +7,7 @@ CREATE TABLE members
     members_seq        INT             NOT NULL, 
     members_name       VARCHAR2(20)    NOT NULL, 
    	members_email      VARCHAR2(20)    NOT NULL,
-    members_pw         VARCHAR2(20)    NOT NULL, 
+    members_pw         VARCHAR2(20)    NOT NULL,
+    members_EmailChecked    VARCHAR2(5),
     CONSTRAINT MEMBERS_PK PRIMARY KEY (members_seq)
 );
